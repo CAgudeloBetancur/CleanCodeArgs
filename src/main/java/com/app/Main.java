98a13d4ac7326e6abbfc29759bbfd67507d1c10d
CAgudeloBetancur <camiloab97@gmail.com>
2025-12-25 21:07:43 -0500

    App execution code setup for testing

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -6,6 +6,23 @@ import com.app.args.Args;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Args arg = new Args("l,p#,d*", args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            executeApplication(logging, port, directory);
+        } catch (Exception e) {
+            System.out.printf("Argument error: %s\n", e.getMessage());
+        }
+    }
+
+    private static void executeApplication(boolean logging, int port, String directory) {
+        if (logging) {
+            System.out.println("--- Modo de registro (Logging) activado ---");
+        }
+
+        System.out.printf("Iniciando servidor en el puerto: %d\n", port);
+        System.out.printf("Directorio de trabajo: %s\n", directory);
     }
 }
